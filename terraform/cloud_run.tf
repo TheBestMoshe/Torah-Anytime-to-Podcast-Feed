b902f367219f4c7e1af90076be0724e8a5d01ace
TheBestMoshe <34072688+TheBestMoshe@users.noreply.github.com>
2021-01-11 20:07:41 -0500

    ci(terraform): Added beta rss service (#4)

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -43,3 +43,36 @@ output "latest_created_revision_name" {
 output "latest_ready_revision_name" {
   value = google_cloud_run_service.rss-service.status[0].latest_ready_revision_name
 }
+
+resource "google_cloud_run_service" "beta-rss-service" {
+  name     = "beta-rss-service"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        # image = "gcr.io/google-samples/hello-app:2.0"
+        image = "gcr.io/${var.project_name}/main_server:beta"
+      }
+      timeout_seconds = 50
+
+    }
+
+    metadata {
+
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+
+
+  depends_on = [google_project_service.run]
+}
+
+output "beta-url" {
+  value = google_cloud_run_service.beta-rss-service.status[0].url
+}
